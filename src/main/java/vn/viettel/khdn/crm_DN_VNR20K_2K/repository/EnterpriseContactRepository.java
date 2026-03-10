3b1095b1e3aea12576653b6a071871615ab4df5a
nhandeptraiii <thanhnhan21082003@gmail.com>
2026-03-10 21:22:16 +0700

    repository: add EnterpriseContactRepository

---
+++ src/main/java/vn/viettel/khdn/crm_DN_VNR20K_2K/repository/EnterpriseContactRepository.java
@@ -0,0 +1,16 @@
+package vn.viettel.khdn.crm_DN_VNR20K_2K.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import vn.viettel.khdn.crm_DN_VNR20K_2K.model.EnterpriseContact;
+
+@Repository
+public interface EnterpriseContactRepository extends JpaRepository<EnterpriseContact, Long> {
+
+    List<EnterpriseContact> findByEnterpriseIdOrderByIsPrimaryDescFullNameAsc(Long enterpriseId);
+
+    void deleteAllByEnterpriseId(Long enterpriseId);
+}

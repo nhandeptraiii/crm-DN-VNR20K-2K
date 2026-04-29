a5a1efb9c7f6b29e4511fc1115b803b359c5abdc
nhandeptraiii <thanhnhan21082003@gmail.com>
2026-04-29 10:14:14 +0700

    feat: add CorsConfig to enable cross-origin requests for frontend domains

--- src/main/java/vn/viettel/khdn/crm_DN_VNR20K_2K/config/CorsConfig.java
+++ src/main/java/vn/viettel/khdn/crm_DN_VNR20K_2K/config/CorsConfig.java
@@ -13,7 +13,12 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "http://localhost:5173",
+                "http://crmkhdncto.id.vn", // Thay bằng tên miền thật của bạn
+                "https://crmkhdncto.id.vn" // Dòng quan trọng nhất cho HTTPS
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
